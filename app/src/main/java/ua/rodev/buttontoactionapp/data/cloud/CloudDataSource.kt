10b6f3db18f34e37ce933e636940d2762ed8ee76
Rostyslav Holub <kloosprog@gmail.com>
2022-11-28 16:25:03 +0200

    create TestDataSource

--- app/src/main/java/ua/rodev/buttontoactionapp/data/cloud/CloudDataSource.kt
+++ app/src/main/java/ua/rodev/buttontoactionapp/data/cloud/CloudDataSource.kt
@@ -1,10 +1,26 @@
 package ua.rodev.buttontoactionapp.data.cloud
 
+import com.google.gson.Gson
+import ua.rodev.buttontoactionapp.R
 import ua.rodev.buttontoactionapp.data.FetchActions
+import ua.rodev.buttontoactionapp.domain.ReadRawResource
 
 interface CloudDataSource : FetchActions {
 
     class Main(private val service: ActionService) : CloudDataSource {
         override suspend fun fetchActions(): List<ActionCloud> = service.fetchActions()
+    }
+
+    class Test(
+        private val readRawResource: ReadRawResource,
+    ) : CloudDataSource {
+
+        private val gson = Gson()
+
+        override suspend fun fetchActions(): List<ActionCloud> {
+            val json = readRawResource.read(R.raw.test_actions)
+            val fromJson = gson.fromJson(json, CloudActionsList::class.java)
+            return fromJson.toTypedArray().toList()
+        }
     }
 }
--- app/src/main/java/ua/rodev/buttontoactionapp/data/di/CloudModule.kt
+++ app/src/main/java/ua/rodev/buttontoactionapp/data/di/CloudModule.kt
@@ -4,18 +4,26 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import ua.rodev.buttontoactionapp.BuildConfig
 import ua.rodev.buttontoactionapp.data.cloud.ActionService
 import ua.rodev.buttontoactionapp.data.cloud.CloudDataSource
+import ua.rodev.buttontoactionapp.domain.ReadRawResource
 import javax.inject.Singleton
 
 @InstallIn(SingletonComponent::class)
 @Module
 object CloudModule {
 
+    // TODO Create some "real/fake instances holder" to avoid many conditions with BUILD_TYPE
+
     @Provides
     @Singleton
     fun provideCloudDataSource(
         service: ActionService,
-    ): CloudDataSource = CloudDataSource.Main(service)
+        readRawResource: ReadRawResource
+    ): CloudDataSource = when (BuildConfig.BUILD_TYPE) {
+        "TEST" -> CloudDataSource.Test(readRawResource)
+        else -> CloudDataSource.Main(service)
+    }
 
 }
